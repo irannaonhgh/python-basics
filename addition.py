1a0f44a71d33e785faca63a6f5a3b966a9267daf
irannaonhgh <irannadhavalagi25@gmail.com>
2026-03-06 09:30:52 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,5 +1,6 @@
 A = int(input("Enter your first number"))
-b = int(input("Enter your Second number"))
+B = int(input("Enter your Second number"))
 
 Print(" your additon of number is")
 print(A+B)
+
